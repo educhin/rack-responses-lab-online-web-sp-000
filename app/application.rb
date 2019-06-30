ad3f9ccf08bfdb584911e44edb667931461130e3
Chinedu Arinze <c.s.arinze@gmail.com>
2019-06-30 23:54:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,13 @@
 class Application
 
   def call(env)
+    resp = Rack::Response.new
+    time = Time.new
 
+    if time.hour < 11
+      resp.write "Good Morning"
+    else
+      resp.write "Good Afternoon"
   end
 
 end
